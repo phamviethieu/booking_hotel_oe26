dcaaae7bb3a3cf21588897e83742878ec2452ef5
phamviethieu <pviethieu@gmail.com>
2020-06-17 08:55:31 +0700

    update user, booking_details, comment  tables

---
+++ database/migrations/2020_06_16_071900_update_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('users', 'role_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->integer('role_id')
+                    ->unsigned()
+                    ->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('users', 'role_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('role_id')
+                    ->change();
+            });
+        }
+    }
+}
